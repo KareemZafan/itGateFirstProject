644bc6bfa3a67beb10bb38096a356986a4d590cd
KareemZafan <kareemm.zafan@gmail.com>
2023-12-13 18:45:14 +0200

    applying review comments

---
+++ src/math.py
@@ -0,0 +1,5 @@
+    
+def add (a, b):
+    return a + b 
+
+print(f"Sum = {add(4,5)}")
--- src/pythongCode.py
+++ src/pythongCode.py
@@ -6,9 +6,3 @@ ls = ["Ahmed", "Mahmoud", "Kareem", "Osama", "Ali"]
 for i in ls: 
     print(i)
     
-    
-def add (a, b):
-    return a + b 
-
-
-print(add(4,5))
